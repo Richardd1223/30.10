81b301a43404289b37d5eb20b5a2ccd0eed88d74
Richard Čokina <richardcokina@MacBook-Air-uzivatela-Richard.local>
2024-10-30 11:07:48 +0100

    update

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+public class Employee implements Person {
+        private String name;
+        private String surname;
+        private int age;
+        private int yearsToWork;
 
-public class Employee {
-}
+        public Employee(String name, String surname, int age) {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.yearsToWork = 62 - this.age;  // Predpokladaný vek odchodu do dôchodku je 62
+        }
+
+        @Override
+        public void getInfo() {
+            System.out.println("Employee Name: " + name);
+            System.out.println("Employee Surname: " + surname);
+            System.out.println("Employee Age: " + age);
+            System.out.println("Years to work: " + yearsToWork + "\n");
+        }
+    }
+
+
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+public class Student implements Person {
+    private String studentID;
+    private String name;
+    private String surname;
+    private int age;
 
-public class Student {
+    public Student(String studentID, String name, String surname, int age) {
+        this.studentID = studentID;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student Name: " + name);
+        System.out.println("Student Surname: " + surname);
+        System.out.println("Student Age: " + age + "\n");
+    }
 }
+
